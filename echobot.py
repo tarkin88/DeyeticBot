fdb92c2f41dc2a6d2905406c6d66a4065ddd84cb
Antonio de Mora <antonio.dmora@gmail.com>
2015-08-17 02:47:33 -0500

    Crea bot DeyeticBot usando python-telegram-bot

---
+++ echobot.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#
+# Simple Bot to reply Telegram messages
+# Copyright (C) 2015 Leandro Toledo de Souza <leandrotoeldodesouza@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
+
+
+import logging
+import telegram
+import time
+
+
+LAST_UPDATE_ID = None
+
+
+def main():
+    global LAST_UPDATE_ID
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Telegram Bot Authorization Token
+    bot = telegram.Bot('57983814:AAFLTwqiXUmXuKbOdntR_cMAGmtxgqxtvnM')
+
+    # This will be our global variable to keep the latest update_id when requesting
+    # for updates. It starts with the latest update_id if available.
+    try:
+        LAST_UPDATE_ID = bot.getUpdates()[-1].update_id
+    except IndexError:
+        LAST_UPDATE_ID = None
+
+    while True:
+        echo(bot)
+        time.sleep(3)
+
+
+def echo(bot):
+    global LAST_UPDATE_ID
+
+    # Request updates from last updated_id
+    for update in bot.getUpdates(offset=LAST_UPDATE_ID):
+        if LAST_UPDATE_ID < update.update_id:
+            # chat_id is required to reply any message
+            chat_id = update.message.chat_id
+            sender = update.message.from_user #['from']['first_name']
+            print(sender.first_name)
+            # message = update.message.text.encode('utf-8')
+       
+            message = "Deja de estar mamando, " + sender.first_name
+            print(type(message))
+
+            if (message):
+                # Reply the message
+                bot.sendMessage(chat_id=chat_id,
+                                text=message)
+
+                # Updates global offset to get the new updates
+                LAST_UPDATE_ID = update.update_id
+
+
+if __name__ == '__main__':
+    main()
